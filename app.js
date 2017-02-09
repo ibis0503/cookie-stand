832dff12edd1763999d7da140dbf01693953c125
ibis0503 <kennyhong@outlook.com>
2017-02-09 01:06:51 -0800

    generate a random number of customers per hours based on min / max houly customers

---
+++ app.js
@@ -0,0 +1,18 @@
+// Stores the min/max hourly customers, and the average cookies per customer, in object properties
+var pikePlace = {
+	min: 17,
+	max: 88,
+	avg: 5.2,
+  // Uses a method of that object to generate a random number of customers per hour.
+	getRandom: function(min, max) {
+		return Math.floor(Math.random() * (max-min + 1)) + min;
+	}
+}
+pikePlace.getRandom(pikePlace.min, pikePlace.max)
+// pikePlace.getRandom(pikePlace.min, pikePlace.max)* 5.2 = simulated amounts of cookies purchased for each hour
+
+// Using for loop to calculate for each hours
+
+// Display the values of each array as unordered lists in the browser
+
+// Display the lists on index.html.
