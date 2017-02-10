41a27959fc594a3cac75e7da2330cc01163bf538
ibis0503 <kennyhong@outlook.com>
2017-02-10 00:12:03 -0800

    create li element for each of data point and appned the li to ul

--- app.js
+++ app.js
@@ -7,19 +7,23 @@ var pikePlace = {
 	getRandom: function(min, max) {
 		return Math.random() * (max-min + 1) + min;
 	},
-	hour: ['10am - 11am : ','11am - 12pm : ', '12pm - 1pm : ','1pm - 2pm : ','2pm - 3pm : ','3pm - 4pm : ','4pm - 5pm : ']
-};
-
-// Function for displaying the values of each array as lists in the browser
-function cookies_purchased_hour() {
-	// Using for loop to display for each hours
-	for (var i = 0; i < 7; i++)  {
-		// pikePlace.getRandom(pikePlace.min, pikePlace.max)* 5.2 = simulated amounts of cookies purchased for each hour
-		document.write(pikePlace.hour[i] + Math.floor(pikePlace.getRandom(pikePlace.min, pikePlace.max)*(pikePlace.avg))+ '<br />');
+	hour: ['10am - 11am : ','11am - 12pm : ', '12pm - 1pm : ','1pm - 2pm : ','2pm - 3pm : ','3pm - 4pm : ','4pm - 5pm : ', 'Total : '],
+	render: function() {
+		// Get an element from Id
+		var parent = document.getElementById('parentElement');
+		// Using for loop to display for each hours and getRandom
+		for (var i = 0; i < 7; i++)	{
+			// Get an element from li
+			var child = document.createElement('li');
+			// Create and populate an LI element for each of the data points that you want to render
+			child.textContent = pikePlace.hour[i] + Math.floor(this.getRandom(pikePlace.min, pikePlace.max)*(pikePlace.avg));
+			// Append the li to ul
+			parent.appendChild(child);
+		}
+			var total = document.createElement('li');
+			total.textContent = pikePlace.hour[7] + Math.floor(this.getRandom(pikePlace.min, pikePlace.max)*(pikePlace.avg));
+			parent.appendChild(total);
 	}
-}
-
-var liEl = document.getElementById("hour"); // This is a reference to the li element
-hour.textContent = cookies_purchased_hour();
-
+};
+pikePlace.render();
 // var pikePlaceHourlyTotal = pikePlaceHourlyTotal1 + pikePlaceHourlyTotal2
