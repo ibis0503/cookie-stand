9e182405b2cbbac5fe36f82b470f60972543251d
ibis0503 <kennyhong@outlook.com>
2017-02-11 16:10:10 -0800

    auto indent

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// build Constructor Functions
 function cookieStand(name, min, max, avg) {
 	this.name = name;
 	this.min = min;
@@ -6,8 +7,8 @@ function cookieStand(name, min, max, avg) {
 	this.sale = [];
 	this.totalsale = 0;
 	this.getRandom = function(min, max) {
-			return Math.random() * (max-min + 1) + min;
-		};
+		return Math.random() * (max-min + 1) + min;
+	};
 	this.getHourlySale = function() {
 		for (var i = 0; i < 7; i++) {
 			var total = Math.floor(this.getRandom(this.min, this.max) * this.avg);
@@ -17,6 +18,7 @@ function cookieStand(name, min, max, avg) {
 			this.totalsale += total;
 		}
 	};
+	// create input inside of HTML tables
 	this.renderAsRow = function() {
 		var cookieTable = document.getElementById('cookieTable');
 		var trEl = document.createElement('tr');
@@ -24,10 +26,7 @@ function cookieStand(name, min, max, avg) {
 		placeName.textContent = this.name;
 		trEl.appendChild(placeName);
 		cookieTable.appendChild(trEl);
-		//
 		this.getHourlySale();
-		// Get an element from Id
-
 		// Using for loop to display for each hours and getRandom
 		for (var i = 0; i < 7; i++)	{
 			// Get an element from li
@@ -38,7 +37,6 @@ function cookieStand(name, min, max, avg) {
 			trEl.appendChild(cookieStandData);
 			cookieTable.appendChild(trEl);
 		}
-
 		var total = document.createElement('td');
 		// Create and populate an total element for sales out of array
 		total.textContent = this.totalsale;
@@ -46,13 +44,13 @@ function cookieStand(name, min, max, avg) {
 		cookieTable.appendChild(trEl);
 	}
 };
-
+// Set a variable
 var pikePlace = new cookieStand('Pike Place', 17, 88, 5.2);
 var seaTac = new cookieStand('SeaTac Airport', 6, 24, 1.2);
 var southCenter = new cookieStand('Southcenter', 11, 38, 1.9);
 var bellevueSquare = new cookieStand('Bellevue Square', 11, 38, 1.9);
 var alki = new cookieStand('Alki', 3, 24, 2.6);
-
+// Make a call
 pikePlace.renderAsRow();
 seaTac.renderAsRow();
 southCenter.renderAsRow();
