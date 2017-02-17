be863987a14c753a04c9dcdd5aac658a21254ec0
Kenny Hong <kennyhong@outlook.com>
2017-02-16 20:17:26 -0800

    started lap09

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict';
 // build Constructor Functions
 function cookieStand(name, min, max, avg) {
 	this.name = name;
@@ -9,7 +10,9 @@ function cookieStand(name, min, max, avg) {
 	this.getRandom = function(min, max) {
 		return Math.random() * (max-min + 1) + min;
 	};
-	this.getHourlySale = function() {
+
+	// Create prototype for hourly sale
+	cookieStand.prototype.getHourlySale = function() {
 		for (var i = 0; i < 7; i++) {
 			var total = Math.floor(this.getRandom(this.min, this.max) * this.avg);
 			// Total value will add into sale array upto 6times
@@ -18,8 +21,9 @@ function cookieStand(name, min, max, avg) {
 			this.totalsale += total;
 		}
 	};
-	// create input inside of HTML tables
-	this.renderAsRow = function() {
+
+	// Create prototype for generate input inside of HTML tables
+	cookieStand.prototype.renderAsRow = function() {
 		var cookieTable = document.getElementById('cookieTable');
 		var trEl = document.createElement('tr');
 		var placeName = document.createElement('td');
@@ -44,6 +48,7 @@ function cookieStand(name, min, max, avg) {
 		cookieTable.appendChild(trEl);
 	}
 };
+
 // Set a variable
 var pikePlace = new cookieStand('Pike Place', 17, 88, 5.2);
 var seaTac = new cookieStand('SeaTac Airport', 6, 24, 1.2);
@@ -56,3 +61,27 @@ seaTac.renderAsRow();
 southCenter.renderAsRow();
 bellevueSquare.renderAsRow();
 alki.renderAsRow();
+
+//declaire the form
+var cookieForm = document.getElementById('form');
+//generate input into table
+function generateCookieForm(event) {
+	event.preventDefault();
+	var store_name = document.getElementById('name');
+	var min_cust = document.getElementById('min');
+	var max_cust = document.getElementById('max');
+	var avg_cookie = document.getElementById('avg');
+	//adding the newCookieStore to the cookieStandsArray
+	var newCookieStore = new cookieStand(store_name, min_cust, max_cust, avg_cookie);
+	// Make a call
+	newCookieStore.renderAsRow();
+	//we're emptying the form
+	event.target.name.value = null;
+	event.target.min.value = null;
+	event.target.max.value = null;
+	event.target.avg.value = null;
+}
+//listening for the click and running handler above
+cookieForm.addEventListener('submit', generateCookieForm);
+console.log(pikePlace.name);
+console.log(newCookieStore.name);
